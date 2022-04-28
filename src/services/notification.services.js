830eba40d25decbbdcb3af0d688a6172c17ae5c6
Pradeep Kshirsagar <30392189+pradeep99909@users.noreply.github.com>
2022-04-28 21:25:24 +0530

    notification email

--- src/services/notification.services.js
+++ src/services/notification.services.js
@@ -1,18 +1,31 @@
+require("dotenv").config();
+
 const nodemailer = require("nodemailer");
 const userServices = require("./user.services");
 const sendEmail = async () => {
+  console.log("-----sendEmail ---");
   const users = await userServices.listUsers();
+
   for (let i = 0; i < users.length; i++) {
+    //console.log("sendEmail ::users ::", users[i]);
+    console.log(
+      "sendEmail :: process.env.GAMIL_ADDRESS ::",
+      process.env.GMAIL_ADDRESS
+    );
+    console.log(
+      "sendEmail :: process.env.GMAIL_PASSWORD ::",
+      process.env.GMAIL_PASSWORD
+    );
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.GAMIL_ADDRESS,
+        user: process.env.GMAIL_ADDRESS,
         pass: process.env.GMAIL_PASSWORD,
       },
     });
 
     const mailOptions = {
-      from: "youremail@gmail.com",
+      from: process.env.GMAIL_ADDRESS,
       to: users[i].email,
       subject: "Sending Email using Node.js",
       text: "That was easy!",
@@ -23,6 +36,11 @@ const sendEmail = async () => {
 };
 
 const TransportMail = (transporter, mailOptions) => {
+  console.log(
+    "TransportMail :: transporter, mailOptions",
+    transporter,
+    mailOptions
+  );
   return new Promise((resolve, reject) => {
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
